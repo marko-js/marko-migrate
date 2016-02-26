4324751c272841503d86537833c0f3fc5fd8ddb5
Patrick Steele-Idem <pnidem@gmail.com>
2016-02-26 15:39:44 -0700

    Added transformer for html-doctype attr

---
+++ src/transformers/html-doctype-attr.js
@@ -0,0 +1,12 @@
+'use strict';
+
+exports.transform = function(el, attr, context) {
+    el.removeAttribute('html-doctype');
+
+    var doctype = attr.value.value;
+
+    var builder = context.builder;
+
+    var doctypeText = builder.text(builder.literal('<!DOCTYPE ' + doctype + '>\n'));
+    el.insertSiblingBefore(doctypeText);
+};
